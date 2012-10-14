1b56af82a154d90fa03b7990571812722939a3b5
Felix Mohnert <felix.mohnert@absolventa.de>
2012-10-14 20:58:38 +0200

    bro validations

--- app/models/bro.rb
+++ app/models/bro.rb
@@ -10,4 +10,9 @@ class Bro < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar, :firstname, :lastname
   has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "200x200>" }
 
+  before_save
+  validates :firstname, :lastname, :presence => true
+  validates :email, :presence => true, :uniqueness => true
+  validates :password, :length => { :in => 6..20 }
+  validates_attachment :avatar, :presence => true, :size => { :in => 0..5.megabytes }
 end
