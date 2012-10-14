cec12bcb4d4fd8f386c334cfbdb623a4816414bf
Felix Mohnert <felix.mohnert@absolventa.de>
2012-10-14 18:45:12 +0200

    add logout functionality

--- app/models/bro.rb
+++ app/models/bro.rb
@@ -7,7 +7,7 @@ class Bro < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar, :firstname, :lastname
   has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "200x200>" }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Movember::Application.routes.draw do
   root :to => 'home#index'
   devise_for :bros
 
+  devise_scope :bros do
+    delete "sign_out", :to => "devise/sessions#destroy"
+  end
+
   match '/bros/:id' => 'bros#show'
 
   # The priority is based upon order of creation:
