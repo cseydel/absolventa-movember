97e431bf60c74eff417c74a6aa697e1ce0e51211
Felix Mohnert <felix.mohnert@absolventa.de>
2012-10-14 12:20:16 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Movember::Application.config.secret_token = '06a4cb152f951aff7e2b5f80639598b194392230cd313ba1551b8952cf4fabe8bcbb6d0b22486bd5a7a3b93dd76bcdbaba9e71dde14478941e4911af6aa0bfb5'
